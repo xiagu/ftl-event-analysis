09ac869c90c2eac26966b2ea85cafcb4c6e70e07
xiagu <me@andrew.rs>
2019-11-02 15:33:17 -0700

    Add shareReplay to sectorData

--- src/app/app_providers.ts
+++ src/app/app_providers.ts
@@ -1,7 +1,8 @@
 import {from} from 'rxjs';
+import {shareReplay} from 'rxjs/operators';
 
 import {xhrLoad} from './tools/xhr_load';
 
 export function sectorDataFactory() {
-  return from(xhrLoad('/assets/data/sector_data.xml'));
+  return from(xhrLoad('/assets/data/sector_data.xml')).pipe(shareReplay());
 }
