b57ba0f1a305235d13ba0e39e3db4e196c377c94
xiagu <me@andrew.rs>
2019-11-02 17:18:34 -0700

    Display the event names of the selected sector.

---
+++ src/app/ftl_tags.ts
@@ -0,0 +1,6 @@
+/** XML tag names in FTL data files. */
+export enum FTLTags {
+  EVENT = 'event',
+  EVENT_LIST = 'eventList',
+  SECTOR_DESCRIPTION = 'sectorDescription',
+}
--- src/app/sector-stats/sector-stats.component.ts
+++ src/app/sector-stats/sector-stats.component.ts
@@ -1,8 +1,10 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {MatButtonToggleChange} from '@angular/material/button-toggle';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {map, withLatestFrom} from 'rxjs/operators';
 
 import {SECTOR_DATA_TOKEN} from '../app_tokens';
+import {FTLTags} from '../ftl_tags';
 
 @Component({
   selector: 'ftl-sector-stats',
@@ -10,11 +12,42 @@ import {SECTOR_DATA_TOKEN} from '../app_tokens';
   styleUrls: ['./sector-stats.component.scss']
 })
 export class SectorStatsComponent {
-  readonly sectorNames = this.sectorData.pipe(
-      map((doc) => Array.from(doc.querySelectorAll('sectorDescription'))),
-      map((sectors) => sectors.map((sector) => sector.getAttribute('name'))),
+  readonly sectorNames: Observable<string[]> = this.sectorData.pipe(
+      map((sectorDoc) => {
+        return Array
+            .from(sectorDoc.querySelectorAll(FTLTags.SECTOR_DESCRIPTION))
+            .map((sector) => sector.getAttribute('name'))
+            .filter((name): name is string => !!name);
+      }),
+  );
+
+  /**
+   * Has to be a BehaviorSubject so the ngFor inside the ngIf will get a
+   * replayed value on subscribe.
+   *
+   * With NgRx this would live in the app state, I think? Should also be
+   * reflected in a route.
+   */
+  private readonly selectedSector = new BehaviorSubject<string>('');
+
+  readonly sectorEvents: Observable<string[]> = this.selectedSector.pipe(
+      withLatestFrom(this.sectorData),
+      map(([sector, sectorDoc]) => {
+        const description = sectorDoc.querySelector(
+            `${FTLTags.SECTOR_DESCRIPTION}[name=${sector}]`);
+        if (!description) throw new Error(`Couldn't find sector ${sector}`);
+        return Array.from(description.querySelectorAll(FTLTags.EVENT));
+      }),
+      map((events) => {
+        return events.map((e) => e.getAttribute('name'))
+            .filter((name): name is string => !!name);
+      }),
   );
 
   constructor(@Inject(SECTOR_DATA_TOKEN) private readonly sectorData:
                   Observable<XMLDocument>) {}
+
+  sectorChanged(change: MatButtonToggleChange): void {
+    this.selectedSector.next(change.value);
+  }
 }
